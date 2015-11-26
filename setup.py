9bcbdc470abfff4ffb249bf6611c45698e80ac8a
Rodrigo Souza <rsouza01@gmail.com>
2015-11-26 00:16:18 -0200

    Setup created.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compact_stars_utils',
+      version='0.1',
+      description='Compact stars utils',
+      url='https://github.com/rsouza01/compact.stars.utils',
+      author='Rodrigo Souza',
+      author_email='rsouza01@gmail.com',
+      license='GNU2',
+      packages=['compact_stars_utils'],
+      zip_safe=False)
